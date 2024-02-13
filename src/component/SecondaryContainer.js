e2393b0dbb902fc8f5e270500db21309aa541fd4
niranjan2052 <34083712+niranjan2052@users.noreply.github.com>
2024-02-13 10:39:41 +0545

    Move list added and video link added to video background

--- src/component/SecondaryContainer.js
+++ src/component/SecondaryContainer.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import { useSelector } from "react-redux";
+import React from "react";
+import MovieList from "./MovieList";
 
 function SecondaryContainer() {
+  const movies = useSelector((store) => store?.movies);
   return (
-    <div>SecondaryContainer</div>
-  )
+    (Object.keys(movies).length!==0) && (
+      <div className="bg-black text-white">
+        <div className="ml-4 -mt-64 relative z-20">
+          <MovieList title={"Now Playing"} movies={movies?.nowPlayingMovies} />
+          <MovieList title={"Popular"} movies={movies?.popularMovies} />
+          <MovieList title={"Top Rated"} movies={movies?.topRatedMovies} />
+          <MovieList title={"Upcoming"} movies={movies?.upcomingMovies} />
+
+        </div>
+        {/* 
+      MovieList - Popular
+      Cards
+      MovieList - NowPlaying
+      MovieList - Trending
+      MovieList - Horror
+    */}
+      </div>
+    )
+  );
 }
 
-export default SecondaryContainer
+export default SecondaryContainer;
