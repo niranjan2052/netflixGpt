31b4572931414f08aa127d1be382aeecee039412
niranjan2052 <34083712+niranjan2052@users.noreply.github.com>
2024-02-13 22:54:24 +0545

    Error page added to the react-router

--- src/component/Body.js
+++ src/component/Body.js
@@ -2,6 +2,7 @@ import React from "react";
 import Login from "./Login";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Browse from "./Browse";
+import Error from "./Error";
 
 
 const Body = () => {
@@ -14,6 +15,10 @@ const Body = () => {
       path: "/browse",
       element: <Browse />,
     },
+    {
+      path:"/error",
+      element:<Error/>,
+    }
   ]);
 
  
