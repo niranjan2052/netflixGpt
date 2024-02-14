bfc994268e33fc17bbd3839dfa9b90745fe0e69e
niranjan2052 <34083712+niranjan2052@users.noreply.github.com>
2024-02-14 22:10:37 +0545

    changes made for hosting

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'netflixColor':'#E50914',
+      }
+    },
   },
   plugins: [
     function ({ addUtilities }) {
